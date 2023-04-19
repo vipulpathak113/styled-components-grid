7f58e215844d8ad752100cb13fa7fc9f9a265cd5
Vipul Pathak <vipulpathak113@gmail.com>
2023-04-19 21:56:17 +0530

    attributes styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { useState } from 'react'
-import styled from 'styled-components'
+import styled from "styled-components";
 
 function App() {
-
   const ButtonStyled = styled.button`
-  width: 100px;
-  color: ${(props)=>props.bg?"white":"black"};;
-  background-color: ${(props)=>props.bg?props.bg:"red"};
-  `
+    width: 100px;
+    color: ${(props) => (props.bg ? "white" : "black")};
+    background-color: ${(props) => (props.bg ? props.bg : "khaki")};
+  `;
 
-  const ButtonExtended = styled(ButtonStyled)`   // here we extended the styles previously used
+  const ButtonExtended = styled(ButtonStyled)`
+    // here we extended the styles previously used
     border-radius: 50px;
-  `
-  const ButtonPsuedo = styled(ButtonStyled)`   
-  &:hover{
-    cursor: grabbing;
-  }
-`
+  `;
+  const ButtonPsuedo = styled(ButtonStyled)`
+    &:hover {
+      cursor: grabbing;
+    }
+  `;
+
+  const ButtonAttributes = styled(ButtonStyled).attrs({ type: "submit" })`
+    background-color: green;
+  `;
 
   return (
     <div className="App">
       <ButtonStyled>Button1</ButtonStyled>
       <ButtonStyled bg="blue">Button2</ButtonStyled>
+
       <ButtonExtended>Button extended</ButtonExtended>
       <ButtonPsuedo>Psuedo button</ButtonPsuedo>
+      <ButtonAttributes>Button Attributes</ButtonAttributes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
