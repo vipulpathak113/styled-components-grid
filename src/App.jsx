59257e80d57e455e8b9533a04d923e4c6e357b96
Vipul Pathak <vipulpathak113@gmail.com>
2023-04-20 00:03:12 +0530

    animation with keyframes styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 function App() {
   const ButtonStyled = styled.button`
@@ -18,17 +18,33 @@ function App() {
   `;
 
   const ButtonAttributes = styled(ButtonStyled).attrs({ type: "submit" })`
+    // here we can also use function to recieve props
     background-color: green;
+  `;
+
+  const rotate = keyframes`
+   from {
+    transform: rotate(0deg);
+   }
+   to {
+    transform: rotate(360deg);
+   }
+  `;
+
+  const ImageAnimated = styled.img`
+    animation: ${rotate} 10s linear infinite;
+    width:50px;
+    height: 50px;
   `;
 
   return (
     <div className="App">
-      <ButtonStyled>Button1</ButtonStyled>
-      <ButtonStyled bg="blue">Button2</ButtonStyled>
-
+      <ButtonStyled>Button</ButtonStyled>
+      <ButtonStyled bg="blue">Button Props</ButtonStyled>
       <ButtonExtended>Button extended</ButtonExtended>
-      <ButtonPsuedo>Psuedo button</ButtonPsuedo>
+      <ButtonPsuedo>Button Psuedo</ButtonPsuedo>
       <ButtonAttributes>Button Attributes</ButtonAttributes>
+      <ImageAnimated src="/vite.svg" />
     </div>
   );
 }
